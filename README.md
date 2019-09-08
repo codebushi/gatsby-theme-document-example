45f01ecdf9583d1f1e6e513c258ecc14835c0cf8
Hunter Chang <hunter@madwiremedia.com>
2019-09-08 16:26:19 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gatsby Theme Document Example
---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Document by Code Bushi`,
+    name: `Code Bushi`,
+    siteUrl: `https://codebushi.com`,
+    description: `This is my description that will be used in the meta tags and important for search results`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/codebushi`
+      },
+      {
+        name: `twitter`,
+        url: `https://twitter.com/HuntaroSan`
+      }
+    ],
+    sidebarConfig: {
+      forcedNavOrder: ['/introduction', '/codeblock'],
+      ignoreIndex: true
+    }
+  },
+  plugins: [{ resolve: `gatsby-theme-document`, options: {} }]
+};
