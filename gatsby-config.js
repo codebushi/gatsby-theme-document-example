ad2336343417503de13b9c9f0af0e37bd14e92dc
Hunter Chang <hunter@madwiremedia.com>
2019-09-08 18:33:27 -0600

    netlify url

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Document by Code Bushi`,
     name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.surge.sh`,
+    siteUrl: `https://gatsby-theme-document.netlify.com`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     social: [
       {
